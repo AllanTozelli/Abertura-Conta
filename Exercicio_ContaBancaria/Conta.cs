1d590b7f498c0c5f50d381c27936307670a27fa9
Allan Toelli <allan_tozelli@hotmail.com>
2022-09-20 15:54:51 -0300

    Conta Bancaria

---
+++ Exercicio_ContaBancaria/Conta.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Exercicio_ContaBancaria
+{
+    internal class Conta
+    {
+        public int NumConta { get; private set; }
+        public  string NomeTitular { get; set; }    
+        //public double ValorDepositoInicial { get; set; } // Opcional
+
+        //public double ValorDeposito { get; set; }
+
+        //public double ValorSaque { get; set; }  
+        public double SaldoConta   { get; private set; }
+
+        //public char HaveraDeposito;
+
+        private int Taxa = 5;
+
+
+        public Conta()
+        {
+        }
+
+        public Conta(int numConta, string nomeTitular)
+        {
+            NumConta = numConta;
+            NomeTitular = nomeTitular;
+        }
+
+        public Conta(int numConta, string nomeTitular, double valorDepositoInicial) : this(numConta, nomeTitular)
+        {
+            Deposito(valorDepositoInicial);
+            //NumConta = numConta;
+            //NomeTitular = nomeTitular;
+        }
+
+        //public int NumConta
+        //{
+        //    get { return _numConta; }
+        //    private set
+        //    {
+        //        _numConta = value;
+        //    }
+        //}
+
+
+
+
+        //public void DepositoInicial(char resp)
+        //{
+        //    if (resp == 's' || resp == 'S')
+        //    {
+        //        Console.Write("Entre o valor de depósito inicial: ");
+        //        ValorDepositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        //        SaldoConta = ValorDepositoInicial + SaldoConta;
+                
+        //    } 
+        //}
+
+        public void Deposito(double quantia)
+        {
+            SaldoConta += quantia;        
+        }
+
+        public void Saque(double quantia)
+        {
+             SaldoConta -= (quantia + Taxa);
+        }
+
+
+
+
+            public override string ToString()
+        {
+            return "Conta "
+                + NumConta
+                + ", Titular:  "
+                + NomeTitular
+                + ", Saldo: $ "
+                + SaldoConta.ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+
+    }
+    }
+
---
+++ Exercicio_ContaBancaria/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_ContaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c = new Conta();
+
+
+            
+
+            Console.Write("Entre o número da conta: ");
+            int NumConta = int.Parse(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string NomeTitular = Console.ReadLine();
+            Console.Write("Haverá deposito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double ValorDepositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                c = new Conta(NumConta, NomeTitular, ValorDepositoInicial);
+            }
+            else
+            {
+                c = new Conta(NumConta, NomeTitular);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Dados da Conta:");
+            Console.WriteLine(c.ToString());
+            Console.WriteLine("");
+            Console.Write("Entre um valor para depósito: ");
+            double ValorDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Deposito(ValorDeposito);
+            Console.WriteLine("Dados da Conta atualizados:");
+            Console.WriteLine(c.ToString());
+            Console.WriteLine("");
+            Console.Write("Entre um valor para saque: ");
+            double ValorSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            c.Saque(ValorSaque);
+            Console.WriteLine(c.ToString());
+            Console.ReadLine();
+        }
+    }
+}
